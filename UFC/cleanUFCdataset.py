88995ec3423ce89be0b534e27320086927e6ddbc
danielnieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2020-11-14 14:45:37 +0000

    Choose column names to use in our NN

--- UFC/cleanUFCdataset.py
+++ UFC/cleanUFCdataset.py
@@ -1,2 +1,4 @@
+
 # Columns from csv we wish to use:
-column_names = [""]
+column_names = ["Winner", "B_current_lose_streak", "B_current_win_streak", "B_longest_win_streak", "B_losses", "B_wins", "B_Height_cms", "B_Reach_cms",	"B_Weight_lbs", "R_current_lose_streak", "R_current_win_streak", "R_longest_win_streak", "R_losses", "R_wins", "R_Height_cms", "R_Reach_cms", "R_Weight_lbs", "B_age", "R_age"]
+

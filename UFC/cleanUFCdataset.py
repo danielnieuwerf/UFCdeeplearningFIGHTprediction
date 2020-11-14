ea55fe07a477f43fe2b9d6a8020cbc6a64289dbf
danielnieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2020-11-14 15:28:00 +0000

    Load csv file into pandas dataframe

--- UFC/cleanUFCdataset.py
+++ UFC/cleanUFCdataset.py
@@ -1,9 +1,10 @@
-# Columns from csv we wish to use:
-column_names = ["Winner", "B_current_lose_streak", "B_current_win_streak", "B_longest_win_streak", "B_losses", "B_wins", "B_Height_cms", "B_Reach_cms",	"B_Weight_lbs", "R_current_lose_streak", "R_current_win_streak", "R_longest_win_streak", "R_losses", "R_wins", "R_Height_cms", "R_Reach_cms", "R_Weight_lbs", "B_age", "R_age"]
-
-
 # Load csv file into pandas dataframe
 import pandas as pd
 
-df = pd.read_csv("UFC/UFCdata.csv", delimiter = ",")    #load dataframe from csv
-print(df)   # Print dataframe
+# Column names we wish to use in our NN:
+column_names = ["Winner", "B_current_lose_streak", "B_current_win_streak", "B_longest_win_streak", "B_losses", "B_wins", "B_Height_cms", "B_Reach_cms",	"B_Weight_lbs", "R_current_lose_streak", "R_current_win_streak", "R_longest_win_streak", "R_losses", "R_wins", "R_Height_cms", "R_Reach_cms", "R_Weight_lbs", "B_age", "R_age"]
+
+# load dataframe from UFCdata.csv only using columns specified in column_names
+df = pd.read_csv("UFC/UFCdata.csv", delimiter = ",", usecols= column_names)
+# print(df)   # Print dataframe
+
